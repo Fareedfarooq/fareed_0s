1a993c52964ea58c38e3b6bc143e61e3be2373bc
Haris <hh063063063@gmail.com>
2023-09-15 05:42:40 -0700

    work completed

---
+++ app.js
@@ -0,0 +1,92 @@
+// Q1
+
+var StudentName =[]
+ console.log(StudentName)
+// Q2
+
+var Sir =[   ]
+
+console.log(Sir)
+
+// Q3
+
+var arr1 =["Hello words"]
+
+console.log(arr1)
+
+// Q4
+
+var arr2 =[2,3,4]
+
+console.log(arr2)
+
+// Q5
+
+var arr3 =[true,false]
+
+console.log(arr3)
+
+// Q6
+
+var arr4 =["Hello",1,true]
+
+console.log(arr4)
+
+// Q7
+
+var Qualification =["SSC","HSC","BCS","BS","BCOM","MS","M. Phil.","PHD"]
+document.write("<b>" + "Qualifications;"+ "</b>" + "<br>" + "<br>" + "1) " + Qualification[0] + "<br>" + "2) " + Qualification[1] + "<br>" + "3) " + Qualification[2] + "<br>" + "4) " + Qualification[3] + "<br>" + "5) " + Qualification[4] + "<br>" + "6) " + Qualification[5] + "<br>" + "7) " + Qualification[6] + "<br>" + "8) " + Qualification[7],"<br>")
+
+
+// Q8
+
+
+var storefriends =["ali","Anus","Hamid"]
+
+var store =[320,230,480]
+
+document.write("<br>"+"<br>" + "<b>"+"Score of"+ " "  +storefriends[0]+ " " + store[0]+ " " +"Percentage : 64%" )
+document.write("<br>"+ "Score of" + " " +storefriends[1]+ " "  +store[1]+ " " +"Percentage : 46%" ) 
+document.write("<br>"+"Score of" + " " +storefriends[2]+ " "  +store[2]+ " " +"Percentage : 96%"+"</b>" ) 
+
+
+// Q9
+
+var arr6 = []
+
+arr6.unshift("Red", "Yellow", "Purple")
+
+arr6.push("Green")
+
+arr6.shift()
+
+arr6.pop()
+
+arr6.splice(1, 1, "Gray", "White")
+
+console.log(arr6);
+
+// Q10
+
+var arr7 = [320,230,480,120]
+
+var arr8 = [120,230,320,480]
+
+document.write("<br>"+ "<br>"+ "Scores of students : " + arr7 + "<br>" + "Ordered Scores of Students : " + arr8 + "<br>")
+
+// Q11
+
+var cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+
+document.write("<br>"+ "<b>" + "Cities list:" + "</b>" + "<br>" + cities)
+
+var cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+
+document.write("<br>" + "<b>" + "Selected Cities list:" + "</b>" + "<br>" + cities.slice(1, 3),"<br>")
+
+
+// Q12
+
+var welled = [This ,  is ,  my ,  cat];
+
+document.write[]
